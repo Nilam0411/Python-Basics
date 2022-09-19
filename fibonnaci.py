19f153186a1a3f80ff23238c9c267979d04f51a3
Nilam Sonawane <113552428+Nilam0411@users.noreply.github.com>
2022-09-19 15:39:27 +0530

    Add files via upload

---
+++ fibonnaci.py
@@ -0,0 +1,13 @@
+# easiest way to print fibonacci series
+
+def fib(n):
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c = a+b
+        a = b
+        b = c
+        print(c)
+fib(5)
