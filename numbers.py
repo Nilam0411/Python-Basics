b09c68d58b26c500b2a96fe668b19ade2a43d3ec
Nilam Sonawane <113552428+Nilam0411@users.noreply.github.com>
2022-09-20 17:10:25 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,23 @@
+# wap to print numbers from 1 to 10
+count = 1
+while count<=10:
+    print(count)
+    count+=1
+
+#wap to print numbers from 10 to 1
+count = 10
+while count>0:
+    print(count)
+    count-=1
+
+# wap to print numbers from -1 to -10
+count = -1
+while count>=-10:
+    print(count)
+    count-=1
+
+#wap to print number from -10 to -1
+count = -10
+while count<=-1:
+    print(count)
+    count+=1
